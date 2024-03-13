7155677d083ce08cd635bfc556e36482c72bc7c8
plam3nk <114010915+plam3nk@users.noreply.github.com>
2024-03-14 01:00:08 +0200

    added workshop

---
+++ 04. Workshop Components/workshop-components/src/app/themes-list/themes-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Theme } from '../types/theme';
+
+@Component({
+  selector: 'app-themes-list',
+  templateUrl: './themes-list.component.html',
+  styleUrls: ['./themes-list.component.css'],
+})
+export class ThemesListComponent implements OnInit {
+  themes: Theme[] | null = [];
+  isLoading: boolean = true;
+
+  constructor(private api: ApiService) {}
+
+  ngOnInit(): void {
+    this.api.getThemes().subscribe((themes) => {
+      this.themes = themes;
+      this.isLoading = false;
+    });
+  }
+}
