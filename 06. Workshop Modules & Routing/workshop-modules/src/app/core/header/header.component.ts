c4d9d4b8088e8e0a43ed166cf670e70a06c3d87b
plam3nk <114010915+plam3nk@users.noreply.github.com>
2024-03-16 03:08:02 +0200

    added workshop

---
+++ 06. Workshop Modules & Routing/workshop-modules/src/app/core/header/header.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/user/user.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent {
+  get isLoggedIn(): boolean {
+    return this.userService.isLogged;
+  }
+
+  get firstName(): string {
+    return this.userService.user?.firstName || '';
+  }
+
+  constructor(private userService: UserService, private router: Router) {}
+
+  logout() {
+    this.userService.logout();
+    this.router.navigate(['/home']);
+  }
+}
---
+++ 06. Workshop Modules & Routing/workshop-modules/src/app/theme/posts-list/posts-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/api.service';
+import { Post } from 'src/app/types/post';
+
+@Component({
+  selector: 'app-posts-list',
+  templateUrl: './posts-list.component.html',
+  styleUrls: ['./posts-list.component.css'],
+})
+export class PostsListComponent implements OnInit {
+  posts: Post[] = [];
+  isLoading: boolean = true;
+
+  constructor(private api: ApiService) {}
+
+  ngOnInit(): void {
+    this.api.getPosts(5).subscribe((posts) => {
+      this.posts = posts;
+
+      this.isLoading = false;
+    });
+  }
+}
